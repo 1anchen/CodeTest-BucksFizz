85413469d59b0f9e8b17338f2d6486aedcf6e143
1anchen <svgyc@hotmail.co.uk>
2018-08-02 22:16:57 +0100

    Create BucksFizz Class

---
+++ src/main/java/BucksFizz.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class BucksFizz {
+
+    private ArrayList<Integer> numbers;
+
+
+    public BucksFizz(ArrayList<Integer> numbers){
+        this.numbers = numbers;
+    }
+
+    public ArrayList<Integer> getGetNumbers() {
+        return this.numbers;
+    }
+
+
+}
